8acfadb974a0d6ed54ffb7bf7784ba58af4a20aa
valeriocorrera <valerio.correra@hotmail.com>
2021-02-24 14:23:26 +0100

    1

---
+++ src/app/features/home/home-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './components/home.component';
+
+
+const HOME_ROUTES: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(HOME_ROUTES)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
